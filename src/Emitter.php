e78a4dd64f07838796e47842793cdef15a888a4e
d2gin <672636920@qq.com>
2022-07-11 21:58:47 +0800

    add

---
+++ src/Emitter.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace icy8\EventBus;
+
+class Emitter
+{
+    /**
+     * @var array[][] 事件队列
+     */
+    protected $events = [];
+
+    public function on($name, $event = null, $once = false)
+    {
+        if (!$event) {
+            $this->events[$name][] = ['event' => $event, 'once' => $once];
+        } else if (is_array($name)) {
+            $this->events = array_merge($this->events, $name);
+        }
+        return $this;
+    }
+
+    public function once($name, $event = null)
+    {
+        return $this->on($name, $event, true);
+    }
+
+    public function off($name, $handle = null)
+    {
+        if ($handle) {
+            foreach ($this->events as $n => $es) {
+                foreach ($es as $k => $e) {
+                    if ($handle == $e['event']) {
+                        unset($this->events[$name][$k]);
+                    }
+                }
+            }
+        } else unset($this->events[$name]);
+        return $this;
+    }
+
+    public function trigger($name)
+    {
+        $queue = $this->events[$name] ?? [];
+        if (!is_array($queue)) {
+            return true;
+        }
+        $result = [];
+        $param  = array_slice(func_get_args(), 1);
+        foreach ($queue as $list) {
+            foreach ($list as $key => $row) {
+                $event = $row['event'];
+                $once  = $row['once'];
+                $catch = call_user_func_array($event, $param);
+                if ($once) {
+                    unset($this->events[$name][$key]);
+                }
+                if ($catch === false) {
+                    return $catch;
+                }
+                $result[] = $catch;
+            }
+        }
+        return $result;
+    }
+}
